7064c1a70e50507e2df227f315ab2af04e99355d
zLy28 <zzzzlygood@gmail.com>
2020-11-10 19:44:26 +1100

    some more questions added

--- src/Arrays_101/ArraysProblems.java
+++ src/Arrays_101/ArraysProblems.java
@@ -9,9 +9,10 @@ import java.util.List;
 public class ArraysProblems {
     @Test
     public void test() {
-     
+
     }
-//    Given a binary array, find the maximum number of consecutive 1s in this array.
+
+    //    Given a binary array, find the maximum number of consecutive 1s in this array.
 //    Input: [1,1,0,1,1,1]
 //    Output: 3
 //    Explanation: The first two digits or the last three digits are consecutive 1s.
@@ -115,5 +116,83 @@ public class ArraysProblems {
         }
         Arrays.sort(B);
         return B;
+    }
+
+
+//    Given a fixed length array arr of integers, duplicate each occurrence of zero, shifting the remaining elements to the right.
+//
+//        Note that elements beyond the length of the original array are not written.
+//
+//        Do the above modifications to the input array in place, do not return anything from your function.
+//        Example 1:
+//        Input: [1,0,2,3,0,4,5,0]
+//        Output: null
+//        Explanation: After calling your function, the input array is modified to: [1,0,0,2,3,0,0,4]
+
+//        Example 2:
+//        Input: [1,2,3]
+//        Output: null
+//        Explanation: After calling your function, the input array is modified to: [1,2,3]
+//
+//        Note:
+//        1 <= arr.length <= 10000
+//        0 <= arr[i] <= 9
+    public void duplicateZeros(int[] arr) {
+        int dups = 0;
+        int length_ = arr.length - 1;
+        
+        //find 0s needed to duplicate
+        for (int left = 0; left <= length_-dups; left++) {
+            if (arr[left] == 0) {
+                //find the edge situations: 0 on the boundaries
+                if (left == length_ - dups) {
+                    arr[length_] = 0;
+                    length_ -= 1;
+                    break;
+                }
+                dups++;
+            }
+        }
+        
+        //assign values from the right to the left
+        int last = length_ - dups;
+        for (int i = last; i >=0 ; i--) {
+            if (arr[i] == 0) {
+                arr[i + dups] = 0;
+                dups -= 1;
+                arr[i + dups] = 0;
+            } else {
+                arr[i + dups] = arr[i];
+            }
+        }
+    }
+
+//    Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+//
+//            Note:
+//
+//    The number of elements initialized in nums1 and nums2 are m and n respectively.
+//    You may assume that nums1 has enough space (size that is equal to m + n) to hold additional elements from nums2.
+//    Example:
+//
+//    Input:
+//    nums1 = [1,2,3,0,0,0], m = 3
+//    nums2 = [2,5,6],       n = 3
+//
+//    Output: [1,2,2,3,5,6]
+//
+//
+//    Constraints:
+//
+//            -10^9 <= nums1[i], nums2[i] <= 10^9
+//    nums1.length == m + n
+//    nums2.length == n
+
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        for (int i = 0; i < n; i++) {
+            nums1[m+i]=nums2[i];
+        }
+
+        Arrays.sort(nums1);
     }
 }
